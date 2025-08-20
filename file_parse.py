dcbaed3efc83ee674a5383af5ca8bd9407deb8bf
Ibrahim Ismayilov <ibrahiimismayil@gmail.com>
2025-08-21 00:17:01 +0400

    reading file func completed

--- file_parse.py
+++ file_parse.py
@@ -1,9 +1,4 @@
-# # 1. Develop a function to read file from given file which EEPROM data and contain the data of the wheel components. 
-# # 2. Develop a function that checks whether or not an array of values provide a checksum of 0 or not.
-# # 3. Develop a function that begins to parse using by dividing the data into sections. For every wheel component, there are 3 sections. Each section ends with a checksum padding and a new one begins after up until the third section. Within each of these 3 main sections, each section has a preset amount of mini sections. The first section is called the header and consists of itself. The second section has 4 mini-sections. This includes the date, wheel id, city and country mini-components followed by a checksum phadding. The third and last section has 2 mini components called tray id and part id, which are also followed by the checksum padding. The function must return a boolean value if the cartidges have the correct data and a string otherwise that indicates where each error may be. The function must meet many requirements such as:
-# #           - Calculating the checksums wherever possible, at the end of each of the 3 main sections that contain a checksum padding. 
-# #           - Printing each possible mini-component's data of the wheel on a new line for clarity.
-# #           - Returning a boolean value if the cartidges have the correct data and a string otherwise that indicates where the error is located, in terms of which section or mini-entry.  
+# 1. Develop a function to read file from given file and return a list with seperate Wheel entries
 
 
 # # 1
--- main.py
+++ main.py
@@ -1,11 +1,62 @@
-# 1. Develop a wheel class that will contain properties corresponding to that of an individual EEPROM entry. (Raw EEPROM entry data, number of regions, whether the data can be trusted, wheel id, etc). The number of instances, or wheel objects, will be created based upon how many EEPROM entries are presented by the user. Develop methods for the wheel object as well (checksum, etc)
-# 2. Develop a function parse the file and create an accurate number of wheel objects based on the EEPROM entry. 
+# 1. Develop a function to read file from given file and return a list with seperate Wheel entries
+# 1. Develop a wheel class from which there will be instances created corresponding 
+
+
+# 1. 
+    # 1. Create an empty list that will store seperate EEPROM entries as lists within
+    # 2. Read the file and iterate through it. While all of the file characters are not read, as long as it is not an empty line, create an inner empty list and append EEPROM elements to it. Once there is an empty line, create a new list for a new EEPROM entry and continue the process
+    # 3. Return the list
+
+
+import string
+def read_file(filename: str) -> list:
+    numbers = '1234567890'
+    letters = string.ascii_letters
+    all_wheel_entries = []
+    with open(filename) as file:
+        contents = file.read()
+
+
+    idx = 0
+    wheel_entry = []
+    while idx <= len(contents) - 1:
+        if contents[idx:idx + 2] != '\n\n':
+            if contents[idx] in numbers or contents[idx] in letters:
+                wheel_elem = contents[idx]
+                if contents[idx] in numbers:
+                    while contents[idx + 1] in numbers:
+                        idx += 1
+                        wheel_elem += contents[idx]
+                        if idx == len(contents) - 1:
+                            break
+                wheel_entry.append(wheel_elem)
+        else:
+            all_wheel_entries.append(wheel_entry)
+            wheel_entry = []
+
+        idx += 1
+
+    all_wheel_entries.append(wheel_entry)
+
+
+    return all_wheel_entries
+            
+
+
+            
+
+            
+
+        
+
+
+print(read_file('colleague-file.log'))
+
 
 
 class Wheel:
     def __init__(self):
-        self.header = []
-        self.regions_data = []
+        self.raw_data = []
         self.regions = 0
         self.wheel_id = 0
         self.valid = True
@@ -23,7 +74,12 @@ class Wheel:
         return sum % 256 == 0
     
 
-print(Wheel.get_checksum([9, 37, 0, 0, 0, 0, 0, 0, 210]))
+
+    
+
+# print(Wheel.get_checksum([9, 37, 0, 0, 0, 0, 0, 0, 210]))
+
+
 
 
 
