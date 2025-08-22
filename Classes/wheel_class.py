acb1ed9c2887e5f8626ef9455256cc0c994c5631
Ibrahim Ismayilov <ibrahiimismayil@gmail.com>
2025-08-23 00:03:45 +0200

    new func implemented to analyze regional data

--- Classes/wheel_class.py
+++ Classes/wheel_class.py
@@ -1,17 +1,20 @@
-#  1. Create a wheel class from which a seperate instance will be created for each wheel datagram in the EEPROM entry. The wheel class must have properties like id and validity that will later be displayed to the user. To create an instance, the user only needs parsed data from an EEPROM entry.
-#  2. Create methods that will be used for every wheel instance, like getting the checksum. They will be called in the main.py file for all instances of WheelData. 
-
+#  1. Create a wheel class from which a seperate instance will be created for each wheel datagram in the EEPROM entry. The wheel class must have properties like id and validity that will later be displayed to the user. To create an instance, the user only needs parsed data from an EEPROM entry
+#  2. Create methods that will be used for every wheel instance, like getting the checksum. They will be called in the main.py file for all instances of WheelData
 
 class WheelData:
-    def __init__(self, parsed_data):
+    def __init__(self, parsed_data: list):
+        """
+        Intialize WheelData class with properties that should belong to all wheel datagrams
+        """
 
         self.all_data = parsed_data
         self.header_data = parsed_data[:self.all_data[0]]
 
         self.region_locations = {}
 
-        self.wheel_id = 0
-        self.faulty_region = []
+        self.wheel_id = 'No ID'
+
+        self.faulty_regions = []
 
         self.valid_data = True
 
@@ -22,6 +25,9 @@ class WheelData:
 
     # Time Complexity: O(N), where N is the length of the wheel_section list arguement. Linear time complexity
     def yield_checksum(self, wheel_section: list) -> bool:
+        """
+        Returns whether or not an array of elements yields a checksum of 0
+        """
         sum = 0
         for value in wheel_section:
             if isinstance(value, str):
@@ -32,24 +38,58 @@ class WheelData:
 
         return sum % 256 == 0
     
-    #  1. Check if the checksum of the header section yields 0, proving it is a valid piece of data
-    #  2. Iterate through data element in the header section and assign the locations at which every region starts and ends to the correct region
-    def analyze_header(self): 
 
+    #  1. Check if the checksum of the header section yields 0, proving it is a valid piece of data. If it is provdn valid, continue. Else, end the function as faulty header data means the whole datagram should not be checked further
+    #  2. Iterate through data element in the header section and assign the locations at which every region starts and ends to the correct region by keeping track of the last region checked. One region's beginning signals the previous region's end. All regions are intially presumed to end at the end of the datagram in case no other regions are detected. 
+    
+    #  Time Complexity: O(2N), where N is the length of the header section's contents in the checked wheel entry. Linear Time Complexity
+    def analyze_header(self): 
+        """"
+        Check if the header yields a checksum of 0. If it is valid data, check further and assign beginning and ending indexes of every region
+        """
         if self.yield_checksum(self.header_data) == False:
-            self.valid_data = False
+            self.faulty_regions.append('Header')
 
         else:
+            last_region_checked = 0
             for idx in range(len(self.header_data) - 1):
                 if self.header_data[idx] != 0:
                     if idx != 0:
-                        self.region_locations[idx][1] = self.header_data[idx]
+                        self.region_locations[last_region_checked][1] = self.header_data[idx]
                     self.region_locations[idx + 1] = [self.header_data[idx], len(self.all_data)]  
+                    last_region_checked = idx + 1
+    
+
+    #  1. Iterate through all regions 
+    #  2. If any region does not yield a checksum of 0, add it to the list property that tracks all invalid regions
+    #  3. After iterating, if there were any invalid regions, change valid property of the datagram to False accordingly
+
+    #  Time Complexity: O(N) where N equals the combined length of all the regions in the parsed data. Linear Time Complexity
+    def analyze_regions(self):
+        """
+        Check whether or not the checksum yields 0 all regions of the datagram. Keep track of which regions have invalid data
+        """
+        for region in self.region_locations:
+            region = self.region_locations[region]
+            if self.yield_checksum(self.all_data[region[0]:region[1]]) != True:
+                self.faulty_regions.append(region)
+
+        if len(self.faulty_regions) > 0:
+            self.valid_data = False
+
+    def get_wheel_id(): ...
+
+   
+    def display_mini_entries(self): ...
 
 
-        return self.region_locations
 
 
 new_wheel = WheelData([9, 37, 0, 0, 0, 0, 0, 0, 210, 8, 2, 0, 0, 8, 13, 4, 0, 0, 6, 'W', 'h', 'e', 'e', 'l', 1, 6, 'O', 't', 't', 'a', 'w', 'a', 6, 'C', 'a', 'n', 'a', 'd', 'a', 45, 4, 1, 1, 1, 3, 9, 8, 2, 4, 8, 1, 5, 3, 5, 1, 200])
-print(new_wheel.analyze_header())  # {1: [9, 37], 2: [37, 56]}
 print(new_wheel.yield_checksum(new_wheel.header_data))  # True
+
+new_wheel.analyze_header() 
+print(new_wheel.region_locations)  # {1: [9, 37], 2: [37, 56]}
+
+new_wheel.analyze_regions()
+print(new_wheel.faulty_regions)  # True
--- file_parse.py
+++ file_parse.py
@@ -1,12 +1,18 @@
 # 1. Develop a function to read file from given file and return the contents
 # 2. Develop a function to parse the contents of the file and place them in seperate elements
 
+
 # 1. 
-    # 1. Reads the file and returns the contents stored in a variable
+    #  1. Reads the file and returns the contents stored in a variable
+    #  2. Returns the contents stored in a variable in string format 
 
 
 # Time Complexity: O(N), where N is the amount of characters in the file or the length of the file contents. Linear time complexity
 def read_file(filename: str) -> list:
+    """"
+    Reads the contents of the file and stores it in a variable in string format. Returns the variable to the user
+    """
+
     with open(filename) as file:
         raw_wheel_data = file.read()
 
@@ -16,15 +22,18 @@ file_contents = read_file('colleague-file.log')
 
 
 # 2. 
-    # 1. Create a new list that contain seperate Wheel datagram entries. Import RegEx module to make use of built in functions.
-    # 2. Iterate through all wheel entries and convert all wheel entries into inner lists that contain each data piece as a seperate element by splitting with more than one condition 
-    # 3. Convert all wheel elements that are numbers to integers instead of leaving them as strings. We can do operations on them later if we have to.
+    # 1. Create a new list that contain seperate Wheel datagram entries. Import RegEx module to make use of built in functions
+    # 2. Iterate through all wheel entries and convert all wheel entries into inner lists that contain each data piece as a seperate element by splitting with more than one condition. Through this, only relevant data elements are stored as a seperate element in the wheel entry list and all newline or whitespace characters are disregarded
+    # 3. Convert all wheel elements that are numbers to integers instead of leaving them as strings. This way, operations can be done on them later if needed
     # 4. Return the list containing all wheel entries 
 
 
 # Time Complexity: O(3N), where N is the amount of characters in the file or the length of the contents of the file data given. Linear time complexity
 import re
 def parse_file(raw_wheel_data: str) -> list:
+    """"
+    Seperates wheel entries in the file's contents and seperates each data piece by distinguishing from irrelevant characters. Returns the wheel entries as a list of seperate wheel entry lists
+    """
 
     all_wheel_entries = re.split(r'\n\s*\n', raw_wheel_data)
     
@@ -36,10 +45,8 @@ def parse_file(raw_wheel_data: str) -> list:
             if all_wheel_entries[idx][inner_idx].isdigit():
                 all_wheel_entries[idx][inner_idx] = int(all_wheel_entries[idx][inner_idx])
 
-
     return all_wheel_entries
         
-
 
 print(parse_file(file_contents))  # [['9', '37', '0', '0', '0', '0', '0', '0', '210', '8', '2', '0', '0', '8', '13', '4', '0', '0', '6', 'W', 'h', 'e', 'e', 'l', '1', '6', 'O', 't', 't', 'a', 'w', 'a', '6', 'C', 'a', 'n', 'a', 'd', 'a', '45', '4', '1', '1', '1', '3', '9', '8', '2', '4', '8', '1', '5', '3', '5', '1', '200'], ['9', '37', '0', '0', '0', '0', '0', '0', '210', '8', '2', '0', '0', '8', '13', '4', '0', '0', '6', 'W', 'h', 'e', 'e', 'l', '2', '6', 'O', 't', 't', 'a', 'w', 'a', '6', 'C', 'a', 'n', 'a', 'd', 'a', '44', '4', '1', '1', '1', '4', '9', '8', '2', '4', '8', '1', '5', '3', '5', '1', '199'], ['9', '37', '0', '0', '0', '0', '0', '0', '210', '8', '2', '0', '0', '8', '13', '4', '0', '0', '6', 'W', 'h', 'e', 'e', 'l', '3', '6', 'O', 't', 't', 'a', 'w', 'a', '6', 'C', 'a', 'n', 'a', 'd', 'a', '43', '4', '1', '1', '1', '5', '9', '8', '2', '4', '8', '1', '5', '3', '5', '1', '198'], ['9', '37', '0', '0', '0', '0', '0', '0', '210', '8', '2', '0', '0', '8', '13', '4', '0', '0', '6', 'W', 'h', 'e', 'e', 'l', '4', '6', 'O', 't', 't', 'a', 'w', 'a', '6', 'C', 'a', 'n', 'a', 'd', 'a', '42', '4', '1', '1', '1', '6', '9', '8', '2', '4', '8', '1', '5', '3', '5', '1', '197']]
             
