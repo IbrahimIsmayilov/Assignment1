131e88a134875695e9fda1a24b9f64f874265e29
Ibrahim Ismayilov <ibrahiimismayil@gmail.com>
2025-08-29 16:18:42 -0600

    made getting region idxs more efficient and modular

--- Classes/wheel_class.py
+++ Classes/wheel_class.py
@@ -6,7 +6,6 @@ class WheelData:
         """
         Intialize WheelData class with properties that should belong to all wheel datagrams
         """
-
         self.all_data = parsed_data
         self.header_data = parsed_data[:self.all_data[0]]
 
@@ -18,12 +17,12 @@ class WheelData:
 
         self.valid_data = True
 
-    
+
     #  1. Create a variable to keep track of the region's sum
     #  2. Iterate through all digits in the wheel section and differentiate between letters and numbers. Add the result to the sum variable
     #  3. Return whether or not the sum yields a checksum value of 0
 
-    # Time Complexity: O(N), where N is the length of the wheel_section list arguement. Linear time complexity
+    # Time Complexity: O(N), where N is the length of the wheel_section list argument. Linear time complexity
     def yield_checksum(self, wheel_section: list) -> bool:
         """
         Returns whether or not an array of elements yields a checksum of 0
@@ -33,35 +32,39 @@ class WheelData:
             if isinstance(value, str):
                 sum += ord(value)
             else:
-                sum += value
-                
+                sum += value       
 
         return sum % 256 == 0
     
 
-    #  1. Check if the checksum of the header section yields 0, proving it is a valid piece of data. If it is proven valid, continue. Else, end the function as faulty header data means the whole datagram should not be checked further
-    #  2. Iterate through data element in the header section and assign the locations at which every region starts and ends to the correct region by keeping track of the last region checked. One region's beginning signals the previous region's end. All regions are intially presumed to end at the end of the datagram in case no other regions are detected. Create a condition to check whether or not the iteration is at the first index to avoid any errors when checking for the last region's end. 
+    #  1. Check if the checksum of the header section yields 0, proving it is a valid piece of data. If it is proven valid, continue. Else, end the function as faulty header data means the whole datagram should not be checked further. Else, call the function that gets the beginning and ending indexes of every region in the wheel entry.
     
-    #  Time Complexity: O(2N), where N is the length of the header section's contents in the checked wheel entry. Linear Time Complexity
+    #  Time Complexity: O(N), where N is the length of the header section's contents in the wheel entry. Linear Time Complexity
     def analyze_header(self): 
         """"
         Check if the header yields a checksum of 0. If it is valid data, check further and assign beginning and ending indexes of every region
         """
         if self.yield_checksum(self.header_data) == False:
             self.faulty_regions.append('Header')
-
         else:
-            last_region_checked = 0
+            self.get_region_locations()
+
+
+    #  1. Iterate through data element in the header section and assign the locations at which every region starts and ends to the correct region by keeping track of the last region checked. One region's beginning signals the previous region's end. 
+    #  2. All regions are intially presumed to end at the end of the datagram in case no other regions are detected. Create a condition to check whether or not the iteration is at the first index to avoid any errors when checking for the last region's end. 
+
+    #  Time Complexity: O(N), where N is the length of the header section's contents in the wheel entry. Linear Time Complexity
+    def get_region_locations(self):
+            """
+            If it is valid data, check further and assign beginning and ending indexes of every region
+            """
             for idx in range(len(self.header_data) - 1):
                 if self.header_data[idx] != 0:
+                    region_checked = idx + 1
                     if idx != 0:
-                        self.region_locations[last_region_checked][1] = self.header_data[idx]
-                        self.region_locations[idx + 1] = [self.region_locations[last_region_checked][1], len(self.all_data)]  
-                    else:
-                        self.region_locations[idx + 1] = [self.header_data[idx], len(self.all_data)]  
+                        self.region_locations[region_checked - 1][1] = self.header_data[idx]
+                    self.region_locations[region_checked] = [self.header_data[idx], len(self.all_data)]  
 
-                    last_region_checked = idx + 1
-    
 
     #  1. Iterate through all regions 
     #  2. If any region does not yield a checksum of 0, add it to the list property that tracks all invalid regions
@@ -80,6 +83,7 @@ class WheelData:
         if len(self.faulty_regions) > 0:
             self.valid_data = False
 
+
     #  1. Check if the wheel entry has the first region by only checking the first insertion in the dictionary. If so, continue. Else, the function ends there and nothing is updated
     #  2. Store the first region's data in a new variable to avoid confusion
     #  3. Find the second mini entry and check if the elements making up the word Wheel exist. If so, update the wheel id of the instance with the number proceeding. Else, Label first region as faulty by adding to the faulty regions list and update the validity of the instance accordingly
@@ -90,7 +94,6 @@ class WheelData:
         """
         Get and store the id of the wheel component by analyzing first region. Replace the ASCII character wheel ID with its numeric value
         """
-        
         first_key = next(iter(self.region_locations.keys()))
 
         if first_key == 1:
@@ -108,12 +111,26 @@ class WheelData:
             else:
                 self.faulty_regions.append(1)
                 self.valid_data = False
+
+    
+    #  1. Create a mini entry begin and end variable to keep track of where is being iterated and shown to the user.
+    #  2. Iterate through all regions in the code with a loop
+    #  3. Print the mini entries on different lines for clarity. Continue this while the mini entry end variable does not clash with the ending index of the region
+    # def display_mini_entries(self): 
+    #     """
+    #     Iterate through all regions and display all mini entries on a seperate line for clarity
+    #     """
+
+    #     mini_entry_begin_idx = 0
+    #     mini_entry_end_idx = 0
+
+
+    #     for region in self.region_locations:
+    #         region = self.region_locations[region]
+    #         mini_entry_begin_idx = region[0]
+    #         for mini_entry_elem_idx in range(mini):
             
-                        
-
-
-   
-    def display_mini_entries(self): ...
+            
 
 
 
@@ -132,3 +149,10 @@ print(new_wheel.wheel_id)  # 1
 print(new_wheel.all_data)  # [9, 40, 0, 0, 0, 0, 0, 0, 207, 8, 2, 0, 0, 8, 13, 4, 0, 0, 6, 'W', 'h', 'e', 'e', 'l', 49, 6, 'O', 't', 't', 'a', 'w', 'a', 6, 'C', 'a', 'n', 'a', 'd', 'a', 45, 4, 1, 1, 1, 3, 9, 8, 2, 4, 8, 1, 5, 3, 5, 1, 200]
 new_wheel.analyze_regions()
 print(new_wheel.faulty_regions)  # [1]
+
+
+
+
+
+
+    
